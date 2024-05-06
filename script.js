2fca71c9d81369049a18219e2bc67f42503bf99f
Gabrielle Lommel <granolagabrielle@gmail.com>
2024-05-06 16:14:00 -0500

    buttons wired up and logging

--- script.js
+++ script.js
@@ -1,20 +1,49 @@
 // State Variables can be declared outside of the onReady
 // Feel free to make this to what you want!
 // Example:
-// let fungusHP = 100;
+
+// global variables
+
+let fungusHP = 100;
+let attackAP = 100;
 
 function onReady() {
-    console.log("Ready to go!")
-    
-    // Make sure you check the index.html file! 
-    // There are lots of buttons and things ready for you to hook into here!
-    
-    
-    // 🧠 Remember
-    // - Handle events that ->
-    // - Updates state which is ->
-    // - Rendered to the DOM
+  console.log('Ready to go!');
+
+  // Make sure you check the index.html file!
+  // There are lots of buttons and things ready for you to hook into here!
+
+  // 🧠 Remember
+  // - Handle events that ->
+  // - Updates state which is ->
+  // - Rendered to the DOM
+}
+onReady();
+
+// arcane scepter button function
+
+function useArcaneScepter(event) {
+  event.target;
+  console.log('arcane scepter selected');
 }
 
+// entangle button function
 
-onReady()
+function useEntangle(event) {
+  event.target;
+  console.log('entangle selected');
+}
+
+// dragon-blade button function
+
+function useDragonBlade(event) {
+  event.target;
+  console.log('dragon blade selected');
+}
+
+// star-fire button function
+
+function useStarFire(event) {
+  event.target;
+  console.log('star fire selected');
+}
